96f66bb7cd943c3bee9fcf916cf60c5da2f528a9
Tobias <moosfett86@gmail.com>
2025-09-08 09:05:39 +0200

    Bump version code and version name to 3 and 0.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
     applicationId = "de.moosfett.notificationbundler"
     minSdk = 26
     targetSdk = 35
-    versionCode = 2
-    versionName = "0.1.1"
+    versionCode = 3
+    versionName = "0.2.0"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     vectorDrawables { useSupportLibrary = true }
