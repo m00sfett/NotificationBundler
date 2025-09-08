d3bd5a2f125b601fc6cea8292dfa88770110801a
Tobias <moosfett86@gmail.com>
2025-09-08 08:25:54 +0200

    Image Asset

--- app/src/main/java/de/moosfett/notificationbundler/ui/nav/AppNav.kt
+++ app/src/main/java/de/moosfett/notificationbundler/ui/nav/AppNav.kt
@@ -5,6 +5,7 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
