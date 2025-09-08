a1466a460250e6c5e435ffdc8caedb229b5a199e
Tobias <moosfett86@gmail.com>
2025-09-08 05:53:08 +0200

    Order filter rules by id

--- app/src/main/java/de/moosfett/notificationbundler/data/db/FiltersDao.kt
+++ app/src/main/java/de/moosfett/notificationbundler/data/db/FiltersDao.kt
@@ -6,10 +6,10 @@ import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface FiltersDao {
-    @Query("SELECT * FROM filter_rules")
+    @Query("SELECT * FROM filter_rules ORDER BY id")
     suspend fun all(): List<FilterRuleEntity>
 
-    @Query("SELECT * FROM filter_rules")
+    @Query("SELECT * FROM filter_rules ORDER BY id")
     fun observeAll(): Flow<List<FilterRuleEntity>>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
